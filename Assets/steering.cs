2319d0ecd5ed0b59fd38e48dff20a034ae9ca10e
Noah Seymour <Noah Seymour>
2017-03-03 16:58:39 +1000

    now with trees and steering

---
+++ Assets/steering.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class steering : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.A))
+        {
+            Vector3 newPos = transform.localPosition;
+            if (newPos.x < 2.5f)
+                newPos.x += .1f;
+            transform.localPosition = newPos;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 newPos = transform.localPosition;
+            if(newPos.x >-2.5f)
+            newPos.x -= .1f;
+            transform.localPosition = newPos;
+        }
+    }
+}
