ecb75c6d05c93a8a219d54755f1515ca49ffd3ca
SAE\nlseymour <1005771@student.sae.edu.au>
2017-03-06 18:19:03 +1000

    car smoothing, new sky box

--- Assets/steering.cs
+++ Assets/steering.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class steering : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public float cameraBias = .96f;
+    public float carSmoothing;
+    public float steeringSpeed;
+    public float steeringSpeedLeft;
+    public float steeringSpeedRight;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKey(KeyCode.A))
+        /*if (Input.GetAxis("Horizontal") < 0)
         {
             Vector3 newPos = transform.localPosition;
             if (newPos.x < 2.5f)
-                newPos.x += .1f;
-            transform.localPosition = newPos;
+                newPos.x -= Input.GetAxis("Horizontal") * steeringSpeed;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, carSmoothing * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetAxis("Horizontal") >0)
         {
             Vector3 newPos = transform.localPosition;
             if(newPos.x >-2.5f)
-            newPos.x -= .1f;
-            transform.localPosition = newPos;
-        }
+            newPos.x -= Input.GetAxis("Horizontal")* steeringSpeed;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, carSmoothing * Time.deltaTime);
+        }*/
+
+        //Vector3 newPos = transform.localPosition;
+
+
+        
+        Vector3 clampedPosition = transform.localPosition;
+        clampedPosition.x = Mathf.Clamp(transform.localPosition.x, -3.0f, 3.0f);
+        transform.localPosition = clampedPosition;
+
+        transform.position = transform.position - (transform.right * Input.GetAxis("Horizontal")) * steeringSpeed;
+
+        Vector3 moveCam = transform.position + transform.forward * 8.0f + Vector3.up * 2.5f;
+        Camera.main.transform.position = Camera.main.transform.position * cameraBias + moveCam * (1.0f - cameraBias);
+        Camera.main.transform.LookAt(transform.position + -transform.forward * 30.0f);
+
     }
 }
