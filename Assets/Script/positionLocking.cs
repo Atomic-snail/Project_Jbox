6622343ac5b0a9be29f01f141bfc072eb8633b57
Noah Seymour <nlseymour@sae.au>
2017-03-08 20:12:43 +1000

    fixed the positon locking script

--- Assets/Script/positionLocking.cs
+++ Assets/Script/positionLocking.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class positionLocking : MonoBehaviour {
     public GameObject parent;
-	// Use this for initialization
-	void Start () {
+    public float xmove;
+    public float ymove;
+    public float zmove;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = parent.transform.position;		
+        Vector3 newPos = parent.transform.position;
+        newPos.x += xmove;
+        newPos.y += ymove;
+        newPos.z += zmove;
+        transform.position = newPos;
 	}
 }
