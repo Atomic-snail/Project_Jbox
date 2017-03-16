ae478cc52f5aac05780b3d38609949d5507362ea
Noah Seymour <Noah Seymour>
2017-03-16 23:25:54 +1000

    more adjustments to the generation system

--- Assets/Script/Terraingenerating.cs
+++ Assets/Script/Terraingenerating.cs
@@ -18,6 +18,8 @@ public class Terraingenerating : MonoBehaviour {
     public int Direction; //1 = north, 2 = east, 3 = south, 4 = west
     public float nextAnchorXpos;
     public float nextAnchorZpos;
+    int StartGen;
+    public float timeScale;
 	// Use this for initialization
 	void Start () {
        // DeleteLoop = 0;
@@ -26,8 +28,111 @@ public class Terraingenerating : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.G))
-            animationEnd();
+        //if (Input.GetKeyDown(KeyCode.G))
+        //    animationEnd();
+        if(StartGen <= HillsTimes.Length-4)
+        {
+            int Rand = Random.Range(0, CliffTiles.Length - 2);
+            int rand2 = Random.Range(0, 10);
+            if (loopStop == 0)
+            {
+                loopStop = 10;
+                if (Mathf.Abs(nextAnchorXpos) > maxDistance || Mathf.Abs(nextAnchorZpos) > maxDistance)
+                {
+                    if (Direction == 1)
+                    {
+                        if (nextAnchorXpos < 0)
+                        {
+                            Rand = CliffTiles.Length - 1;
+                            Direction = 2;
+                            Debug.Log("East");
+                        }
+                        else
+                        {
+                            Rand = CliffTiles.Length - 2;
+                            Direction = 4;
+                            Debug.Log("west");
+                        }
+                    }
+                    else if (Direction == 3)
+                    {
+                        if (nextAnchorXpos < 0)
+                        {
+                            Rand = CliffTiles.Length - 2;
+                            Direction = 2;
+                            Debug.Log("East");
+                        }
+                        else
+                        {
+                            Rand = CliffTiles.Length - 1;
+                            Direction = 4;
+                            Debug.Log("west");
+                        }
+                    }
+                    else if (Direction == 2)
+                    {
+                        if (nextAnchorZpos < 0)
+                        {
+                            Rand = CliffTiles.Length - 2;
+                            Direction = 1;
+                            Debug.Log("north");
+                        }
+                        else
+                        {
+                            Rand = CliffTiles.Length - 1;
+                            Direction = 3;
+                            Debug.Log("south");
+                        }
+                    }
+                    else if (Direction == 4)
+                    {
+                        if (nextAnchorZpos < 0)
+                        {
+                            Rand = CliffTiles.Length - 1;
+                            Direction = 1;
+                            Debug.Log("north");
+                        }
+                        else
+                        {
+                            Rand = CliffTiles.Length - 2;
+                            Direction = 3;
+                            Debug.Log("south");
+                        }
+                    }
+                }
+                else
+                {
+                    if (rand2 == 1 && loopStop == 0)
+                    {
+                        Rand = CliffTiles.Length - 1;
+                        loopStop = 10;
+                    }
+                    if (rand2 == 2 && loopStop == 0)
+                    {
+                        Rand = CliffTiles.Length - 2;
+                        loopStop = 10;
+                    }
+                }
+                
+            }
+            StartGen++;
+            newTile = Instantiate(CliffTiles[Rand], nextAnchor.transform.position, nextAnchor.transform.rotation);
+            nextAnchor = newTile.transform.GetChild(1).gameObject;
+            nextAnchorXpos = nextAnchor.transform.position.x;
+            nextAnchorZpos = nextAnchor.transform.position.z;
+            HillsTimes[DeleteLoop] = newTile;
+            pieceList[DeleteLoop] = Rand;
+            DeleteLoop++;
+            if (DeleteLoop > HillsTimes.Length - 1)
+            {
+                DeleteLoop = 0;
+                deleteStart = true;
+            }
+            if (loopStop > 0)
+                loopStop--;
+
+        }
+        Time.timeScale = timeScale;
         //{
         //    if(deleteStart)
         //    Destroy(HillsTimes[DeleteLoop]);
