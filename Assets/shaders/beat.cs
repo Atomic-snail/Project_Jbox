8007339fc42324c7026425f745e963c06223cd10
Unknown <1006878@student.sae.edu.au>
2017-03-20 01:00:37 +1000

    Imported new shader, as well as tree/rock shader.

--- Assets/shaders/beat.cs
+++ Assets/shaders/beat.cs
@@ -2,79 +2,235 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class beat : MonoBehaviour {
+public class beat : MonoBehaviour
+{
     AudioSource source;
 
     float[] spectrum = new float[64];
-    Material mat;
+    Material matCar;
     public GameObject car;
-    public float smoothValue;
-    public float smoothValue2;
+    public float carZScale;
+    public float multiplierPulseCar;
 
-   public bool pulse = false;
-   public bool pulse2 = false;
+
+    public GameObject[] leaves;
+    public List<Material> leavesMat;
+    public GameObject[] rocks;
+    public List<Material> rocksMat;
+    public List<Material> materialsToChange;
+    //Differnt colors to move through
+    public float timeToChangeColor;
+    public float colorTime;
+    public Colors[] songsColors;
+    public int currentColor = 0;
+    public float smoothValue;
+    public Color lerpedColor = Color.white;
+
+    public int currentSong;
+
+
+
+    public bool pulse = false;
+    public bool fade;
+    public float fadeSpeed;
+    public float t;
+
+
+    public bool timerNoMusic;
+    public float timerForPulse;
+    float tp;
 
     public int index = 0;
+
+    [System.Serializable]
+    public struct Colors
+    {
+       
+        public Color[] colors;
+    }
 
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         source = GetComponent<AudioSource>();
-        mat = car.GetComponent<Renderer>().material;
-        float scale = car.transform.lossyScale.z;
-        scale = scale / 2;
-        //mat.SetFloat("_Scale",scale);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        source.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+        matCar = car.GetComponent<Renderer>().material;
+        carZScale = car.transform.lossyScale.z;
+   
+        for (int i = 0; i < leaves.Length; ++i)
+        {
+            Material[] tempMats = leaves[i].GetComponent<Renderer>().materials;
+
+            foreach (Material m in tempMats)
+            {
+                if (m.shader.name == "Custom/tree")
+                {
+                    leavesMat.Add(m);
+                    materialsToChange.Add(m);
+
+                }
+            }
+        }
+
+        for (int i = 0; i < rocks.Length; ++i)
+        {
+            Material[] tempMats = rocks[i].GetComponent<Renderer>().materials;
+            foreach (Material m in tempMats)
+            {
+                if (m.shader.name == "Custom/rock")
+                {
+                    rocksMat.Add(m);
+                    materialsToChange.Add(m);
+
+                }
+            }
+        }
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.GetSpectrumData(spectrum, 0, FFTWindow.Hanning);
+
+         leaves = GameObject.FindGameObjectsWithTag("Trees");
+
+
+       
+
         //Vector3 pos = transform.position;
         //pos.y = pos.y*0.9f + spectrum[0]*0.1f;
         //transform.position = pos;
 
-        if (spectrum[1] > 0.05 && !pulse)
+
+
+        //Changing color
+        colorTime += Time.deltaTime;
+        if (colorTime > timeToChangeColor)
         {
-            pulse = true;
-            smoothValue = 0;
-        }
-        if (pulse)
-        {
-            smoothValue += Time.deltaTime * 4.0f;
-            if (smoothValue > 1.8f)
+            
+            if (currentColor >= songsColors[currentSong].colors.Length - 1)
             {
-                pulse = false;
+                currentColor = 0;
+                
+            }
+            else
+            {
+               
+                ++currentColor;
+            }
+            fade = true;
+            t = 0;
+           
+
+
+            colorTime = 0;
+        }
+        if (fade)
+        {
+            if (currentColor - 1 >= 0)
+            {
+
+                lerpedColor = Color.Lerp(songsColors[currentSong].colors[currentColor - 1], songsColors[currentSong].colors[currentColor], t);
+
+            }
+            else
+            {
+                lerpedColor = Color.Lerp(songsColors[currentSong].colors[songsColors[currentSong].colors.Length-1], songsColors[currentSong].colors[currentColor], t);
+            }
+
+           
+
+            foreach (Material m in materialsToChange)
+            {
+
+                Debug.Log(currentColor);
+                m.SetColor("_Color", lerpedColor);
+
+
+
+            }
+            t += fadeSpeed * Time.deltaTime;
+            if (t >= 1)
+            {
+                t = 0;
+                fade = false;
+            }
+
+        }
+
+        if (!timerNoMusic)
+        {
+
+
+            if (spectrum[0] > 0.12f && !pulse)
+            {
+                pulse = true;
                 smoothValue = 0;
             }
-        }
-
-        mat.SetFloat("_Music", smoothValue);
-
-
-
-
-        if (spectrum[10] > 0.1f && !pulse2)
-        {
-            pulse2 = true;
-            smoothValue2 = 0;
-        }
-        if (pulse2)
-        {
-            smoothValue2 += Time.deltaTime * 20.0f;
-            if (smoothValue2 > 5.0f)
+            if (pulse)
             {
-                pulse2 = false;
-                smoothValue2 = 0;
+                smoothValue += Time.deltaTime * multiplierPulseCar;
+                if (smoothValue > carZScale)
+                {
+                    pulse = false;
+                    smoothValue = 0;
+                }
             }
         }
+        else
+        {
+
+            tp += Time.deltaTime;
+            if (tp >= timerForPulse)
+            {
+                smoothValue += Time.deltaTime * multiplierPulseCar;
+                if (smoothValue > carZScale)
+                {
+                    pulse = false;
+                    smoothValue = 0;
+                }
+            }
+
+
+        }
+        //For car
+        matCar.SetFloat("_Music", smoothValue);
+
+        foreach (Material m in materialsToChange )
+        {
+            m.SetFloat("_Music", smoothValue);
+        }
+
+        //    //For trees
+        //    foreach (Material m in leavesMat)
+        //{
+        //    m.SetFloat("_Music", smoothValue);
+        //    Debug.Log("tress");
+        //}
+        ////For rocks
+        //foreach (Material m in rocksMat)
+        //{
+        //    m.SetFloat("_Music", smoothValue);
+        //    Debug.Log("rocks");
+          
+        //}
+
+
+
+
+
+
         //smoothValue = smoothValue * 0.9f + spectrum[0] * 0.1f;
         //mat.SetFloat("_Music", Mathf.Sin (smoothValue));
-      
-        //mat.SetFloat("_Music2", smoothValue2);
-        for (int i = 1; i<64; ++i)
+
+
+        for (int i = 1; i < 64; ++i)
         {
-            Debug.DrawLine(new Vector3((i - 1)/100f, spectrum[i - 1],0), new Vector3(i/100, spectrum[i], 0), Color.red);
+            Debug.DrawLine(new Vector3((i - 1) / 100f, spectrum[i - 1], 0), new Vector3(i / 100, spectrum[i], 0), Color.red);
         }
-	}
+    }
 }
