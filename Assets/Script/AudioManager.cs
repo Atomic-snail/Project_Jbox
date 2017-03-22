e0fa3b75b070613610ac3858e7e0a57d0e9d8068
SAE\ighadban <1006878@student.sae.edu.au>
2017-03-22 14:58:17 +1000

    Audiomanager script added

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //array of floats necessary for scenery changes.
+    public float[] songTimes;
+    public AudioClip[] songFiles;
+    public float[] songBPMTimer;
+    public float[] timeOfDay;
+
+    public float timeRemaining;
+
+    public AudioSource audioSource;
+
+    public AudioClip randomizedSong;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        randomizedSong = (songFiles[Random.Range(0, songFiles.Length)]);
+
+        for (int i = 0; i < songFiles.Length; i++)
+        {
+            songTimes[i] = songFiles[i].length;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeRemaining = randomizedSong.length;
+
+        timeRemaining -= Time.time;
+
+        if (timeRemaining <= 0.0f) {
+            NewSong();
+        }
+
+    }
+
+    //randomize new song at end of song.
+    void NewSong() {
+        randomizedSong = (songFiles[Random.Range(0, songFiles.Length)]);
+    }
+}
